4107e780d0bc0c89c292203a21ceedfd94cbb376
durgaprasad <durgaprasadgudipalli@gmail.com>
2022-03-23 09:27:55 +0530

    version 3

---
+++ VehicleProject/src/main/java/com/lti/app/service/VehicleServiceImpl.java
@@ -0,0 +1,26 @@
+package com.lti.app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lti.app.dao.VehicleDAO;
+import com.lti.app.pojo.Vehicle;
+@Service
+@Transactional
+public class VehicleServiceImpl implements VehicleService
+{
+	@Autowired
+	VehicleDAO vdao;
+	
+	@Override
+	public boolean addVehicle(Vehicle vehicle) {
+		return vdao.addVehicle(vehicle);
+	}
+	@Override
+	public List<Vehicle> getVehicles(){
+		return vdao.getVehicles();
+	}
+}
